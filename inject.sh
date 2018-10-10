d5d668180c873e261d78ac4ae2f4c7d6b226f2a4
jbuice <jbuice@kainostech.com>
2018-10-10 10:57:32 -0500

    Create inject.sh

---
+++ inject.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+INPUT=tlos_genesis_snapshot_inject.csv
+OLDIFS=$IFS
+IFS=,
+[ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
+while read line eth name token stake key
+do
+echo /telos2/telos2.3/build/programs/teclos/teclos -u http://127.0.0.1:8889 system newaccount -x 100 --transfer --stake-net "\""$stake TLOS"\""  --stake-cpu "\""$stake TLOS"\"" --buy-ram-kbytes 4 eosio $name $key -f | bash -
+#echo /telos2/telos2.2/build/programs/teclos/teclos -u http://127.0.0.1:8889 transfer eosio $name "\""$liquid TLOS"\"" | bash -
+done < $INPUT
+IFS=$OLDIFS
