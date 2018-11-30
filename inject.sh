83e12fd1e6f335d071ca703da62ba0aef6292f8a
J.T. Buice <jbuice@kainostech.com>
2018-11-29 23:52:49 -0600

    Update inject.sh

--- inject.sh
+++ inject.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-INPUT=tlos_genesis_snapshot_inject.csv
+#
+# Script by J.T. Buice - KainosBP.com
+#
+SUM=0
+INPUT=tlos_inject.csv
 OLDIFS=$IFS
 IFS=,
 [ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
-while read line eth name token stake liquid key
+while read name key liquid cpu bw tokenamt
 do
-echo /telos2/telos2.3/build/programs/teclos/teclos -u http://127.0.0.1:8889 system newaccount -x 100 --transfer --stake-net "\""$stake TLOS"\""  --stake-cpu "\""$stake TLOS"\"" --buy-ram-kbytes 4 eosio $name $key -f | bash -
-#echo /telos2/telos2.2/build/programs/teclos/teclos -u http://127.0.0.1:8889 transfer eosio $name "\""$liquid TLOS"\"" | bash -
+echo /telos/telos2.6/build/programs/teclos/teclos -u http://127.0.0.1:8888 system newaccount -x 100 --transfer --stake-net "\""$bw TLOS"\""  --stake-cpu "\""$cpu TLOS"\"" --buy-ram-kbytes 4 eosio $name $key -f | bash -
+#echo /telos2/telos2.5/build/programs/teclos/teclos -u http://127.0.0.1:8888 transfer eosio $name "\""$liquid TLOS"\"" | bash -
 done < $INPUT
 IFS=$OLDIFS
